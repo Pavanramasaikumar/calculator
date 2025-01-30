2d7bb61495c73053dac5f23213aaab807e8d75b8
Pavanramasaikumar <147789485+Pavanramasaikumar@users.noreply.github.com>
2025-01-30 23:32:42 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,77 +1,15 @@
-let a;
-let b;
-let t;
-let r;
-function update() {
-    document.getElementById("res").textContent = r;
+const display=document.getElementById("display");
+function append(input){
+    display.value +=input;
 }
-function getInputValues() {
-    a = parseInt(document.getElementById("a").value);
-    b = parseInt(document.getElementById("b").value);
+function cleardis(){
+    display.value="";
 }
-function getinput(){
-    t=parseInt(document.getElementById("c").value);
-}
-function add() {
-    getInputValues();
-    r=a+b;
-    update();
-}
-function mul() {
-    getInputValues();
-    r=a*b;
-    update();
-}
-function sub() {
-    getInputValues();
-    r=a-b;
-    update();
-}
-function div() {
-    getInputValues();
-    if(b==0){
-        r="division is not posible";
+function equaldis(){
+    try{
+        display.value=eval(display.value);
     }
-    else{
-        r=a/b;
+    catch(error){
+        display.value="error";
     }
-    update();
-}
-function mod() {
-    getInputValues();
-    r=a%b;
-    update();
-}
-function pow(){
-    getInputValues();
-    r=Math.pow(a,b);
-    update();
-}
-function sqrt(){
-    getInputValues();
-    r=Math.sqrt(a);
-    update();
-}
-function log(){
-    getInputValues();
-    r=Math.log10(a);
-    update();
-}
-function sin(){
-    getinput();
-    r=Math.ceil(Math.sin(t));
-    update();
-}
-function cos(){
-    getinput();
-    r=Math.ceil(Math.cos(t));
-    update();
-}
-function tan(){
-    getinput();
-    r=Math.ceil(Math.tan(t));
-    if (r==-1){
-        r="∞";
-    }
-    update();
 }
